0ec2ab8fd95742a1f74c91bd27a1335523899455
Juliano Costa <julianocosta89@outlook.com>
2019-07-04 22:21:49 +0200

    Update cart.component.ts
    Test

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 
-import { CartService } from '../cart.service'; 
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -15,7 +15,7 @@ export class CartComponent implements OnInit {
   constructor(
     private cartService: CartService,
     private formBuilder: FormBuilder
-  ) { 
+  ) {
     this.items = this.cartService.getItems();
 
     this.checkoutForm = this.formBuilder.group({
